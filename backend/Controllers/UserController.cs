941c1907e9f385d95fb3275edee1abb43b37c542
MarsLander81 <gabrielmamon444@gmail.com>
2026-01-06 20:37:50 +0800

    Intial Commit, adjusted models & added new user api

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace task_manager_api.Controllers
+{
+    using TaskManager.Models;
+    using TaskManager.Data;
+
+    [Route("user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+        } 
+    }
+}
+    
