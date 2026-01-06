c38e46531d153c9027034e95f11c8098e2a74ee7
MarsLander81 <gabrielmamon444@gmail.com>
2026-01-06 23:25:02 +0800

    added delete function to user api

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -30,7 +30,21 @@ namespace task_manager_api.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
-        } 
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+
+            _context.Users.Remove(user);
+            // Also delete associated tasks
+            var tasks = _context.Tasks.Where(t => t.UserId == id);
+            _context.Tasks.RemoveRange(tasks);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
     
